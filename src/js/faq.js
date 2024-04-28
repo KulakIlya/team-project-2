d0990ea24f147d92b3eb12af92505d9a4d8318bb
IrinaCota <iryna.kotsa91@gmail.com>
2024-04-28 21:31:56 +0100

    add accordion

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,12 +1,26 @@
 import createAccordion from './accordion';
 
-document.addEventListener("DOMContentLoaded", function () {
-    createAccordion({
-        containerClass: 'faq-list',
-        elementClass: 'faq-item',
-        triggerClass: 'faq-scroll-button',
-        panelClass: 'ac-panel',
+document.addEventListener('DOMContentLoaded', function () {
+  createAccordion({
+    containerClass: 'faq-list',
+    elementClass: 'faq-item',
+    triggerClass: 'faq-scroll-button',
+    panelClass: 'ac-panel',
+  });
+
+  const triggers = document.querySelectorAll('.faq-scroll-button');
+
+  triggers.forEach(function (trigger) {
+    trigger.addEventListener('click', function () {
+        const panel = this.closest('.faq-item').querySelector('.ac-panel');
+
+      if (panel.style.display === 'block' || panel.style.display === '') {
+          panel.style.display = 'none';
+        
+      } else {
+          panel.style.display = 'block';
+          
+      }
     });
+  });
 });
-
-
