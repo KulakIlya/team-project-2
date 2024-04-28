7324d82d96ef2e69e9d7580d2981f5e9377ab3ac
Ilya <kulak1224@gmail.com>
2024-04-28 19:03:16 +0300

    fix markup and styles

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,6 +1,18 @@
+import createAccordion from './accordion';
+
 createAccordion({
   containerClass: 'accordion', // Клас контейнера аккордеона
   elementClass: 'accordion-item', // Клас кожного елементу аккордеона
   triggerClass: 'accordion-button', // Клас кнопки аккордеона
-  panelClass: 'accordion-content' // Клас контенту аккордеона
+  panelClass: 'accordion-content', // Клас контенту аккордеона
+});
+
+const accordion = document.querySelector('.accordion');
+
+accordion?.addEventListener('click', e => {
+  const closest = e.target.closest('.accordion-button');
+
+  if (!closest) return;
+
+  closest.querySelector('.icon-arrow-down').classList.toggle('rotated');
 });
