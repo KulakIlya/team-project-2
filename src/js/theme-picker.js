170017bc275551918902893b98f5d25bdd791740
Ilya <kulak1224@gmail.com>
2024-04-28 10:27:05 +0300

    remove theme picker from index.html and fix theme-picker.js

--- src/js/theme-picker.js
+++ src/js/theme-picker.js
@@ -1,10 +1,10 @@
 const themePicker = document.querySelector('.theme-picker-form');
-const formInputs = themePicker.elements;
+const formInputs = themePicker?.elements;
 
 const storedTheme = localStorage.getItem('picked-theme') ?? 'red';
 
-formInputs[storedTheme].checked = true;
+if (formInputs) formInputs[storedTheme].checked = true;
 
-themePicker.addEventListener('change', e => {
+themePicker?.addEventListener('change', e => {
   localStorage.setItem('picked-theme', e.target.value);
 });
