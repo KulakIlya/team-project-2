60894cf676ad2eb3be1a7ec861e641d754656e8d
AlenaDenysenko <naopiya28@gmai.com>
2024-04-28 15:40:32 +0300

    error correction

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,5 +1,52 @@
 import createSwiper from './swiper';
 
+const reviewsData = [
+    {
+        name: "Natalia",
+        photo: "./images/reviews_natalia@1x.png",
+        text: "Work with was extraordinary! He turned out to be a very competent and responsible specialist. The projects were completed on time and the result exceeded my expectations."
+    },
+    {
+        name: "Dmytro",
+        photo: "./images/reviews_dmytro@1x.png",
+        text: "I have the honor to recommend him as an exceptional professional in his field. His knowledge and expertise are undeniable. Cooperation with him always brings impressive results."
+    },
+    {
+        name: "Anna",
+        photo: "./images/reviews_anna@1x.png",
+        text: "The developed project impresses with its quality and efficiency. The code is cleanly written and the functionality exceeds expectations. Extremely satisfied with the cooperation!"
+    },
+    {
+        name: "Ivetta",
+        photo: "./images/reviews_ivetta@1x.png",
+        text: "Thanks for the excellent work on the project! His talent and professionalism deserve recognition. I recommend it to everyone who is looking for an expert in the field of software development."
+    },
+    {
+        name: "Alex",
+        photo: "./images/reviews_alex@1x.png",
+        text: "Thanks for the excellent work on the project! His talent and professionalism deserve recognition. I recommend it to everyone who is looking for an expert in the field of software development."
+    }
+];
+
+function generateReviewCard(review) {
+    return `
+        <li class="swiper-slide">
+            <img src="${review.photo}" alt="photo" width="48" height="48" loading="lazy">
+            <div class="">
+                <h3 class="reviews-name">${review.name}</h3>
+                <p class="reviews-text">${review.text}</p>
+            </div>
+        </li>
+    `;
+}
+
+const reviewsContainer = document.querySelector('.reviews-ul');
+
+reviewsData.forEach(review => {
+    reviewsContainer.innerHTML += generateReviewCard(review);
+});
+
+
 
 createSwiper({
     swiperContainerClass: 'swiper',
@@ -15,20 +62,10 @@ createSwiper({
 });
 
 const swiper = new Swiper('.swiper-container', {
-    // Вказуємо кількість видимих слайдів для різних роздільностей екрану
     slidesPerView: {
-        // Для мобільних пристроїв
-        480: 1, // Показуємо по одному слайду
-        // Для планшетів
-        768: 2, // Показуємо по два слайди
-        // Для комп'ютерів
-        1200: 4 // Показуємо по чотири слайди
+        480: 1, 
+        768: 2, 
+        1200: 4 
     },
-    // Додаткові налаштування
-    // ...
+   
 });
-
-
-
-
-
