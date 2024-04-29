b12a2685eb5c1f0a60f675c33f7fcb4b2316106d
Ilya <kulak1224@gmail.com>
2024-04-29 12:30:50 +0300

    merge with main

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,13 +1,9 @@
 import Swiper from 'swiper';
-<<<<<<< Updated upstream
 
-import { Navigation } from 'swiper/modules';
-=======
 import 'swiper/css';
 import 'swiper/css/navigation';
 
 import { Manipulation, Navigation } from 'swiper/modules';
->>>>>>> Stashed changes
 
 /**
  * initializes swiper instance
@@ -16,26 +12,17 @@ import { Manipulation, Navigation } from 'swiper/modules';
  * @param {object} options.navigationButtons - object of navigation buttons
  * @param {string} options.prevEl – class of prev button
  * @param {string} options.nextEl – class of next button
-<<<<<<< Updated upstream
-=======
  * @param {number} options.spaceBetween – space between slides
  * @param {object} options.slidesPerView – objects of slides per view
  * @param {number} options.slidesPerView.mobile – number of slides on mobile device
  * @param {number} options.slidesPerView.tablet – number of slides on tablet device
  * @param {number} options.slidesPerView.desktop – number of slides on desktop device
->>>>>>> Stashed changes
  * @returns {Swiper} – created instance
  */
 
 export default function createSwiper({
   swiperContainerClass,
   navigationButtons: { prevEl, nextEl },
-<<<<<<< Updated upstream
-}) {
-  return new Swiper(`.${swiperContainerClass}`, {
-    // Install modules
-    modules: [Navigation],
-=======
   spaceBetween = 30,
   slidesPerView = { mobile: 1, tablet: 1, desktop: 1 },
 }) {
@@ -43,27 +30,10 @@ export default function createSwiper({
     // Install modules
     modules: [Navigation, Manipulation],
     direction: 'horizontal',
->>>>>>> Stashed changes
     speed: 500,
     navigation: {
       nextEl: `.${nextEl}`,
       prevEl: `.${prevEl}`,
     },
-<<<<<<< Updated upstream
-=======
-    spaceBetween,
-    slidesPerView: slidesPerView.mobile,
-    breakpoints: {
-      768: {
-        slidesPerView: slidesPerView.tablet,
-        spaceBetween: slidesPerView.tablet !== 1 ? 16 : spaceBetween,
-      },
-
-      1440: {
-        slidesPerView: slidesPerView.desktop,
-        spaceBetween: slidesPerView.tablet !== 1 ? 16 : spaceBetween,
-      },
-    },
->>>>>>> Stashed changes
   });
 }
