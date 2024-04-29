eb558d8437a542ba891928a4473b5d1663d2e90b
Ilya <kulak1224@gmail.com>
2024-04-29 12:16:08 +0300

    fix swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,7 +2,7 @@ import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-import { Navigation } from 'swiper/modules';
+import { Manipulation, Navigation } from 'swiper/modules';
 
 /**
  * initializes swiper instance
@@ -11,6 +11,11 @@ import { Navigation } from 'swiper/modules';
  * @param {object} options.navigationButtons - object of navigation buttons
  * @param {string} options.prevEl – class of prev button
  * @param {string} options.nextEl – class of next button
+ * @param {number} options.spaceBetween – space between slides
+ * @param {object} options.slidesPerView – objects of slides per view
+ * @param {number} options.slidesPerView.mobile – number of slides on mobile device
+ * @param {number} options.slidesPerView.tablet – number of slides on tablet device
+ * @param {number} options.slidesPerView.desktop – number of slides on desktop device
  * @returns {Swiper} – created instance
  */
 
@@ -18,15 +23,29 @@ export default function createSwiper({
   swiperContainerClass,
   navigationButtons: { prevEl, nextEl },
   spaceBetween = 30,
+  slidesPerView = { mobile: 1, tablet: 1, desktop: 1 },
 }) {
   return new Swiper(`.${swiperContainerClass}`, {
     // Install modules
-    modules: [Navigation],
+    modules: [Navigation, Manipulation],
+    direction: 'horizontal',
     speed: 500,
     navigation: {
       nextEl: `.${nextEl}`,
       prevEl: `.${prevEl}`,
     },
     spaceBetween,
+    slidesPerView: slidesPerView.mobile,
+    breakpoints: {
+      768: {
+        slidesPerView: slidesPerView.tablet,
+        spaceBetween: slidesPerView.tablet !== 1 ? 16 : spaceBetween,
+      },
+
+      1440: {
+        slidesPerView: slidesPerView.desktop,
+        spaceBetween: slidesPerView.tablet !== 1 ? 16 : spaceBetween,
+      },
+    },
   });
 }
